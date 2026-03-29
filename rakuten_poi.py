c38bf4859e49dbd70eaff391ad5c798a1aeca834
kyotosu2023kujo-max <kyotosu2023kujo@gmail.com>
2026-03-29 17:49:00 +0900

    Update rakuten_poi.py

--- rakuten_poi.py
+++ rakuten_poi.py
@@ -26,7 +26,7 @@ def run_rakuten_poi():
         driver.get("https://point.rakuten.co.jp/guidance/login/")
         
         # ログイン処理
-        wait.until(EC.presence_of_element_located("loginInner_u")).send_keys(USER_ID)
+        wait.until(EC.presence_of_element_located((By.ID,"loginInner_u")).send_keys(USER_ID)
         driver.find_element(By.ID, "loginInner_p").send_keys(PASSWORD)
         driver.find_element(By.NAME, "submit").click()
         print("ログインに成功しました。")
